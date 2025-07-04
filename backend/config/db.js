4fe583e56ddb28f1e45740503ae2eea7fff31d3a
Rishabh6355 <rishirai7049@gmail.com>
2025-07-04 15:57:19 +0530

    Initial Structure and Setup

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const connectDB = async ()=>{
+    try{
+        mongoose.connect(process.env.MONGODB_URL)
+        console.log("db connected")
+    }
+    catch(error){
+        console.log("db error")
+    }
+}
+
+export default connectDB
